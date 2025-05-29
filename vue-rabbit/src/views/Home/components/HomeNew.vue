1191dc6fb29d996ee77ab3480f4103d208c64a6a
allen <allen@example.com>
2025-05-29 10:18:12 +0800

    home 新鮮好物實現

--- vue-rabbit/src/views/Home/components/HomeNew.vue
+++ vue-rabbit/src/views/Home/components/HomeNew.vue
@@ -1,156 +1,70 @@
 <script setup>
-import { useCategoryStore } from '@/stores/category'
+import HomePanel from '@/views/Home/components/HomePanel.vue'
+import { findNewAPI } from '@/apis/home'
+import { onMounted, ref } from 'vue'
 
-const categoryStore = useCategoryStore()
+// 获取数据
+const newList = ref([])
+
+const getNewList = async () => {
+  const res = await findNewAPI()
+  console.log(res)
+  newList.value = res.result
+}
+
+onMounted(() => {
+  getNewList()
+})
 </script>
 
 <template>
-  <div class="home-category">
-    <ul class="menu">
-      <li v-for="item in categoryStore.categoryList" :key="item">
-        <RouterLink to="/">{{ item.name }}</RouterLink>
-        <RouterLink v-for="i in item.children.slice(0, 2)" :key="i.id" to="/">{{
-          i.name
-        }}</RouterLink>
-
-        <!-- 弹层layer位置 -->
-        <div class="layer">
-          <h4>分类推荐 <small>根据您的购买或浏览记录推荐</small></h4>
-          <ul>
-            <li v-for="i in item.goods" :key="i.id">
-              <RouterLink to="/">
-                <img :src="i.picture" />
-                <div class="info">
-                  <p class="name ellipsis-2">
-                    {{ i.name }}
-                  </p>
-                  <p class="desc ellipsis">{{ i.desc }}</p>
-                  <p class="price"><i>¥</i>{{ i.price }}</p>
-                </div>
-              </RouterLink>
-            </li>
-          </ul>
-        </div>
+  <HomePanel title="新鲜好物" sub-title="新鲜出炉 品质靠谱">
+    <ul class="goods-list">
+      <li v-for="item in newList" :key="item.id">
+        <RouterLink :to="`/detail/${item.id}`">
+          <img v-img-lazy="item.picture" alt="" />
+          <p class="name">{{ item.name }}</p>
+          <p class="price">&yen;{{ item.price }}</p>
+        </RouterLink>
       </li>
     </ul>
-  </div>
+  </HomePanel>
 </template>
 
 <style scoped lang="scss">
-.home-category {
-  width: 250px;
-  height: 500px;
-  background: rgba(0, 0, 0, 0.8);
-  position: relative;
-  z-index: 99;
+.goods-list {
+  display: flex;
+  justify-content: space-between;
+  height: 406px;
 
-  .menu {
-    li {
-      padding-left: 40px;
-      height: 55px;
-      line-height: 55px;
+  li {
+    width: 306px;
+    height: 406px;
 
-      &:hover {
-        background: $xtxColor;
-      }
+    background: #f0f9f4;
+    transition: all 0.5s;
 
-      a {
-        margin-right: 4px;
-        color: #fff;
+    &:hover {
+      transform: translate3d(0, -3px, 0);
+      box-shadow: 0 3px 8px rgb(0 0 0 / 20%);
+    }
 
-        &:first-child {
-          font-size: 16px;
-        }
-      }
+    img {
+      width: 306px;
+      height: 306px;
+    }
 
-      .layer {
-        width: 990px;
-        height: 500px;
-        background: rgba(255, 255, 255, 0.8);
-        position: absolute;
-        left: 250px;
-        top: 0;
-        display: none;
-        padding: 0 15px;
+    p {
+      font-size: 22px;
+      padding-top: 12px;
+      text-align: center;
+      text-overflow: ellipsis;
+      overflow: hidden;
+      white-space: nowrap;
+    }
 
-        h4 {
-          font-size: 20px;
-          font-weight: normal;
-          line-height: 80px;
-
-          small {
-            font-size: 16px;
-            color: #666;
-          }
-        }
-
-        ul {
-          display: flex;
-          flex-wrap: wrap;
-
-          li {
-            width: 310px;
-            height: 120px;
-            margin-right: 15px;
-            margin-bottom: 15px;
-            border: 1px solid #eee;
-            border-radius: 4px;
-            background: #fff;
-
-            &:nth-child(3n) {
-              margin-right: 0;
-            }
-
-            a {
-              display: flex;
-              width: 100%;
-              height: 100%;
-              align-items: center;
-              padding: 10px;
-
-              &:hover {
-                background: #e3f9f4;
-              }
-
-              img {
-                width: 95px;
-                height: 95px;
-              }
-
-              .info {
-                padding-left: 10px;
-                line-height: 24px;
-                overflow: hidden;
-
-                .name {
-                  font-size: 16px;
-                  color: #666;
-                }
-
-                .desc {
-                  color: #999;
-                }
-
-                .price {
-                  font-size: 22px;
-                  color: $priceColor;
-
-                  i {
-                    font-size: 16px;
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-
-      // 关键样式  hover状态下的layer盒子变成block
-      &:hover {
-        .layer {
-          display: block;
-        }
-      }
+    .price {
+      color: $priceColor;
     }
   }
 }
---
+++ vue-rabbit/src/views/Home/components/HomePanel.vue
@@ -0,0 +1,61 @@
+<script setup>
+/**
+ * 纯展示类组件通用封装思路总结
+ * 1.搭建纯静态的部分，不管可变的部分
+ * 2.抽象可变的部分作为组件参数
+ *   非复杂的模板抽象成props，复杂的解构模板抽象为插槽
+ */
+// 定義props
+defineProps({
+  // 主標題
+  title: {
+    type: String,
+  },
+  // 副標題
+  subTitle: {
+    type: String,
+  },
+})
+</script>
+
+<template>
+  <div class="home-panel">
+    <div class="container">
+      <div class="head">
+        <!-- 主标题和副标题 -->
+        <h3>
+          {{ title }}<small>{{ subTitle }}</small>
+        </h3>
+      </div>
+      <!-- 主体内容区域 -->
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.home-panel {
+  background-color: #fff;
+
+  .head {
+    padding: 40px 0;
+    display: flex;
+    align-items: flex-end;
+
+    h3 {
+      flex: 1;
+      font-size: 32px;
+      font-weight: normal;
+      margin-left: 6px;
+      height: 35px;
+      line-height: 35px;
+
+      small {
+        font-size: 16px;
+        color: #999;
+        margin-left: 20px;
+      }
+    }
+  }
+}
+</style>
